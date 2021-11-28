8b5820580acffc7c5256ec543d9edb2e05afbf8b
Derrc <53236934+Derrc@users.noreply.github.com>
2021-11-28 18:02:19 -0500

    Add files via upload

---
+++ UnoRL/uno_random.py
@@ -0,0 +1,33 @@
+import argparse
+
+import rlcard
+from rlcard.agents import RandomAgent
+from rlcard.utils import set_seed
+
+def run(args):
+    # Make environment
+    env = rlcard.make(args.env, config={'seed': 42})
+    num_episodes = 2
+
+    # Seed numpy, torch, random
+    set_seed(42)
+
+    # Set agents
+    agent = RandomAgent(num_actions=env.num_actions)
+    env.set_agents([agent for _ in range(env.num_players)])
+
+    for episode in range(num_episodes):
+
+        # Generate data from the environment
+        trajectories, player_wins = env.run(is_training=False)
+        # Print out the trajectories
+        print('\nEpisode {}'.format(episode))
+        print(player_wins)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Random Agent Uno Example")
+    parser.add_argument('--env', type=str, default='uno')
+
+    args = parser.parse_args()
+
+    run(args)
